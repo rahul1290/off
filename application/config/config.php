c90a81aed29fa26f05a250215fb960e81b965272
rahul121290 <sinha.rahulsinha.sinha@gmail.com>
2020-06-06 17:59:06 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -26,9 +26,9 @@ date_default_timezone_set('Asia/Kolkata');
 // production url
 //$config['base_url'] = 'http://emp2.ibc24.in/kra/';
 // staging url
-//$config['base_url'] = 'http://192.168.25.237/kra/';
+$config['base_url'] = 'http://192.168.25.237/kra/';
 // local url
-$config['base_url'] = 'http://192.168.25.184:82/off/';
+//$config['base_url'] = 'http://192.168.25.184:82/off/';
 
 
 
