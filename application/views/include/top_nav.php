d45015f48fd0a8c84b2d314b82d9db8cdc95d782
rahul121290 <sinha.rahulsinha.sinha@gmail.com>
2020-06-11 11:47:15 +0530

    Update top_nav.php

--- application/views/include/top_nav.php
+++ application/views/include/top_nav.php
@@ -23,15 +23,15 @@
 			<b class="mr-2"><?php echo $this->session->userdata('username'); ?></b>
 			<i class="far fa-user"></i>
         </a>
-        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+        <!-- div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
 			<a href="#" class="dropdown-item">
 				<div class="media">
-					<img width="45" src="<?php echo $this->config->item('img_url').$this->session->userdata('ecode').'.jpg'; ?>" class="img-circle elevation-4" alt="User Image">
+					<img width="45" src="<?php //echo $this->config->item('img_url').$this->session->userdata('ecode').'.jpg'; ?>" class="img-circle elevation-4" alt="User Image">
 					<div class="media-body">
 						<h3 class="dropdown-item-title">
-							&nbsp;&nbsp;&nbsp;<?php echo $this->session->userdata('username'); ?>
+							&nbsp;&nbsp;&nbsp;<?php //echo $this->session->userdata('username'); ?>
 						</h3>
-						<p class="text-sm">&nbsp;&nbsp;&nbsp;&nbsp;<?php echo $this->session->userdata('designation');?></p>
+						<p class="text-sm">&nbsp;&nbsp;&nbsp;&nbsp;<?php //echo $this->session->userdata('designation');?></p>
 					</div>
 				</div>
 			</a>
@@ -41,7 +41,7 @@
 			<a href="#" class="dropdown-footer">Change Profile</a>
 			<div class="dropdown-divider"></div>
 			<a href="#" class="dropdown-footer">Log Out</a>
-        </div>
+        </div-->
       </li>
 	  <li class="nav-item">
         <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#">
