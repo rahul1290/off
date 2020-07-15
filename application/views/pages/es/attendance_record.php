308a505fa535bc915ded10d8fdd162d5157924f8
rahul121290 <sinha.rahulsinha.sinha@gmail.com>
2020-07-15 12:41:39 +0530

    Update attendance_record.php

--- application/views/pages/es/attendance_record.php
+++ application/views/pages/es/attendance_record.php
@@ -12,6 +12,7 @@
 				<div class="col-sm-6">
 					<ol class="breadcrumb float-sm-right">
 						<li class="breadcrumb-item"><a href="<?php echo base_url();?>">Home</a></li>
+						<li class="breadcrumb-item active">Employee section</li>
 						<li class="breadcrumb-item active">Attendance</li>
 					</ol>
 				</div><!-- /.col -->
